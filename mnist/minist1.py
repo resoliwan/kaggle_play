fdec22a7ace201c74eef12e0e5c301a79e30700f
resoliwan <resoliwan@naver.com>
2018-08-20 15:02:42 +0900

    change to layer

--- mnist/minist1.py
+++ mnist/minist1.py
@@ -29,9 +29,13 @@ def neural_layer(X, n_neurons, activation=None):
 
 
 with tf.name_scope("dnn"):
-  hidden1 = neural_layer(X, n_hidden1, tf.nn.relu)
-  hidden2 = neural_layer(hidden1, n_hidden2, tf.nn.relu)
-  logits = neural_layer(hidden2, n_outputs)
+  # hidden1 = neural_layer(X, n_hidden1, tf.nn.relu)
+  # hidden2 = neural_layer(hidden1, n_hidden2, tf.nn.relu)
+  # logits = neural_layer(hidden2, n_outputs)
+  hidden1 = tf.layers.dense(X, n_hidden1, name="hidden1", activation=tf.nn.relu)
+  hidden2 = tf.layers.dense(hidden1, n_hidden2, name="hidden2", activation=tf.nn.relu)
+  logits = tf.layers.dense(hidden2, n_outputs, name="outputs")
+  y_proba = tf.nn.softmax(logits)
 
 with tf.name_scope("loss"):
   xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y, name="xentopy")
